cc4e8dff64050c6aa3566bb57dc6de7b923b69c8
Song Vu Nguyen <vunnems08@gmail.com>
2022-01-04 12:03:50 -0500

    Add new buttons to exit screen

--- MainMenuButton.gd
+++ MainMenuButton.gd
@@ -7,3 +7,11 @@ func _ready():
 
 func _on_MainMenuButton_pressed():
 	get_tree().change_scene("res://TitleMenu.tscn")
+
+
+func _on_Restart_pressed():
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_Next_pressed():
+	pass # Replace with function body.
